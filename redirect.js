4086014fa264d5ca6b0489c8a48d2c3f22fdef7b
Piero Maddaleni <piero@piemadd.com>
2021-05-14 22:27:39 +0000

    1.4.1 - Fixed XSS vulnerability with redirect method

---
+++ redirect.js
@@ -0,0 +1,11 @@
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+let url_string = Array.from(urlParams.keys())[0];
+
+url = hex2str(a2hex(url_string))
+
+link=document.createElement('meta');
+link.httpEquiv='refresh';
+link.content='0; URL=' + Array.from(urlParams.keys())[0];
+
+document.getElementsByTagName('head')[0].appendChild(link);
