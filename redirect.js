ae8e03e723bccf7492c3d4dabd5fbac02b6abfab
Piero Maddaleni <piero@piemadd.com>
2021-05-14 22:30:26 +0000

    1.4.2 - Fixed trying to redirect with still-encoded URL

--- redirect.js
+++ redirect.js
@@ -6,6 +6,6 @@ url = hex2str(a2hex(url_string))
 
 link=document.createElement('meta');
 link.httpEquiv='refresh';
-link.content='0; URL=' + Array.from(urlParams.keys())[0];
+link.content='0; URL=' + hex2str(a2hex(url_string));
 
 document.getElementsByTagName('head')[0].appendChild(link);
