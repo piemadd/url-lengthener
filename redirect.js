8311d6f1813917baaed28df7cbe9015d43f91c6f
Piero Maddaleni <pieromaddaleni03@gmail.com>
2021-05-13 20:24:44 -0400

    Delete redirect.js

--- redirect.js
+++
@@ -1,5 +0,0 @@
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-let url_string = Array.from(urlParams.keys())[0];
-
-window.location.replace(hex2str(a2hex(url_string)))
